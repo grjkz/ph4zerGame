1a304ba685c095c15bb5f0289b870a9f1e3cee12
Philip Choi <gorejakz@gmail.com>
2016-02-18 19:23:36 -0500

    added menu state

--- assets/javascripts/menu.js
+++ assets/javascripts/menu.js
@@ -1,12 +1,12 @@
 var menuState = {
 	create: function() {
 		// render menu stuff here
-		game.add.text(80,80, 'My First Game', {font: '50px Arial', fill: '#ffffff'})
-		var wkey = game.input.keyboard.addKey(Phaser.Keyboard.W);
-		wkey.onDown.addOnce(this.start, this);
+		Game.add.text(80,80, 'My First Game', {font: '50px Arial', fill: 'orange'});
+		var wkey = Game.input.keyboard.addKey(Phaser.Keyboard.W);
+		wkey.onDown.addOnce(function() {Game.add.text(100,100, 'Initiated', {font: '50px Arial', fill: 'orange'});});
 	},
 
 	start: function() {
-		game.state.start('play')
+		// Game.state.start('play');
 	}
-}
+};
