2c87ab44e10a73a4549960881bc67e22ef388227
Philip Choi <gorejakz@gmail.com>
2016-02-18 20:45:11 -0500

    entering new now enters playState

--- assets/javascripts/menu.js
+++ assets/javascripts/menu.js
@@ -1,12 +1,27 @@
 var menuState = {
 	create: function() {
 		// render menu stuff here
-		Game.add.text(80,80, 'My First Game', {font: '50px Arial', fill: 'orange'});
-		var wkey = Game.input.keyboard.addKey(Phaser.Keyboard.W);
-		wkey.onDown.addOnce(function() {Game.add.text(100,100, 'Initiated', {font: '50px Arial', fill: 'orange'});});
+		Game.add.text(80,80, 'Enter your Alias', {font: '50px Arial', fill: 'orange'});
+		document.getElementById('alias-form').style.visibility = 'visible';
+
+		// submit alias to server
+		document.getElementsByClassName('alias-form')[0].addEventListener("submit", function(e) {
+			e.preventDefault();
+
+			var alias = document.getElementsByClassName('alias')[0].value.trim();
+			if (alias) {
+				socket.emit('add alias', {alias: alias});
+				this.style.visibility = 'hidden';
+			}
+
+		});
+		// start playState when the OK from server has been receieved
+		socket.on('start playState', function() {
+			this.start();
+		}.bind(this));
 	},
 
 	start: function() {
-		// Game.state.start('play');
+		Game.state.start('play');
 	}
 };
