057e4a2e51a2e6f849c3645b2e3db1743483260c
Philip Choi <gorejakz@gmail.com>
2016-02-18 19:24:29 -0500

    added basic connectivity and disconnect handling

---
+++ app_v2.js
@@ -0,0 +1,139 @@
+var express = require('express');
+var app = express();
+var ejs = require('ejs');
+
+var tunnel = app.listen(3000,function() {
+	console.log("listening on 3k");
+});
+var io = require('socket.io')(tunnel);
+
+app.set('view_engine', 'ejs');
+app.use(express.static('assets'));
+
+
+app.get('/', function(req,res) {
+	res.render('index_v2.ejs', {players: 0} );
+});
+
+app.get('/game_v2', function(req,res) {
+	res.render('game_v2.ejs');
+});
+
+
+/////////////////////
+// SOCKET IO STUFF //
+/////////////////////
+
+var Users = {};
+
+io.on('connection', function(client) {
+	// delete user from Users when disconnecting (refreshing page, etc)
+	client.on('disconnect', function() {
+		// emit to all other players to remove disconnecting player
+		delete Users[client.id];
+		console.log(Users);
+		console.log(getUserCount(), "Users Remain")
+	});
+
+	// Add new player to Users
+	Users[client.id] = new Player(client.id);
+	console.log(Users)
+	console.log(getUserCount(), "Users")
+
+	// user enters name and clicks "next"
+	client.on('add alias',function(alias) {
+		Users[client.id].alias = alias;
+	});
+
+	// user clicks "join game"
+	client.on('join game', function() {
+		client.broadcast.emit('add new challenger', {player: Users[client.id]});
+	});
+
+	
+
+
+});
+
+
+
+
+
+///////////////
+// FUNCTIONS //
+///////////////
+
+// player constructor
+function Player(id) {
+	this.id = id;
+	this.alias = "unknown";
+	this.bank = 0;
+	this.x = 0; // coordinates
+	this.y = 0;
+	this.facing = undefined;
+}
+
+
+// create random spawn location
+function spawnPoint() {
+	return {
+		x: Math.floor(Math.random()*1181), // floor makes max range exclusive
+		y: Math.floor(Math.random()*501)
+	};
+}
+
+
+// generate coins
+function genCoins() {
+	var name = '';
+	var timer = 0;
+	var value = 0;
+	var randomNum = Math.floor(Math.random()*100);
+	if (randomNum < 13) {
+		name = "gold_coin";
+		timer = Math.floor(Math.random() * 10000)+5000;
+		value = 500;
+	}
+	else if (randomNum < 50) {
+		name = "silver_coin";
+		timer = Math.floor(Math.random() * 10000)+10000;
+		value = 200;
+	}
+	else {
+		name = "copper_coin";
+		timer = Math.floor(Math.random() * 10000)+10000;
+		value = 50;
+	}
+
+	coin = {
+		x: Math.floor(Math.random()*(1280-32)), // 32 is the size of the coin
+		y: Math.floor(Math.random()*(600-32)),
+		type: name,
+		value: value,
+		coinID: coinCounter,
+		expire: timer
+	};
+	coinCounter++;
+
+	return coin;
+}
+
+
+// get player count
+function getUserCount() {
+	var count = 0;
+	for (var user in Users) {
+		count++;
+	}
+	return count;
+}
+
+
+
+
+
+
+
+
+
+
