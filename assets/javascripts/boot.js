c74eb6170d0555c6b6cf08f1a6d670304a34d663
Philip Choi <gorejakz@gmail.com>
2016-02-18 19:23:02 -0500

    added boot state

--- assets/javascripts/boot.js
+++ assets/javascripts/boot.js
@@ -1,6 +1,6 @@
 var bootState = {
 	create: function() {
-		game.physics.startSystem(Phaser.Physics.ARCADE);
-		game.state.start('load')
+		Game.physics.startSystem(Phaser.Physics.ARCADE);
+		Game.state.start('load');
 	}
-}
+};
